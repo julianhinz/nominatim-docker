e49ce24f50539cccdebd54dc89dfa7f82b968e9f
Julian Hinz <julianhinz@gmail.com>
2018-04-11 23:58:17 +0200

    minor fix

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if ${FILTER}; then
+
   echo "Filtering places."
 
   exec osmconvert /srv/nominatim/src/data.osm.pbf -o=/srv/nominatim/src/data.o5m
   exec osmfilter /srv/nominatim/src/data.o5m --keep="place=*" --drop-author -o=/srv/nominatim/src/data.o5m
   exec osmconvert /srv/nominatim/src/data.o5m -o=/srv/nominatim/src/data.osm.pbf
-  exec rm /srv/nominatim/src/data.o5m
+  #exec rm /srv/nominatim/src/data.o5m
 
 fi
