174ac8aef0e651761ce21569cababf4be27fdaca
Julian Hinz <julianhinz@gmail.com>
2018-04-12 14:19:24 +0200

    fix issues

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 if ${FILTER}; then
-  RUN echo "Filtering places."
-  RUN osmconvert /srv/nominatim/src/data.osm.pbf -o=/srv/nominatim/src/data.o5m
-  RUN echo "Filtering places: Converted pbf to o5m"
-  RUN rm /srv/nominatim/src/data.osm.pbf
-  RUN echo "Filtering places: Deleted pbf"
-  RUN osmfilter /srv/nominatim/src/data.o5m --keep="place=*" --drop-author -o=/srv/nominatim/src/data.o5m
-  RUN echo "Filtering places: Filtered o5m"
-  RUN osmconvert /srv/nominatim/src/data.o5m -o=/srv/nominatim/src/data.osm.pbf
-  RUN echo "Filtering places: Converted o5m back to pbf"
-  RUN rm /srv/nominatim/src/data.o5m
-  RUN echo "Filtering places: Deleted o5m"
-  RUN echo "Filtering places: Done"
+  echo "Filtering places."
+  osmconvert /srv/nominatim/src/data.osm.pbf -o=/srv/nominatim/src/data_old.o5m
+  echo "Filtering places: Converted pbf to o5m"
+  rm /srv/nominatim/src/data.osm.pbf
+  echo "Filtering places: Deleted pbf"
+  osmfilter /srv/nominatim/src/data_old.o5m --keep="place=*" --drop-author -o=/srv/nominatim/src/data.o5m
+  echo "Filtering places: Filtered o5m"
+  rm /srv/nominatim/src/data_old.o5m
+  echo "Filtering places: Deleted pbf"
+  osmconvert /srv/nominatim/src/data.o5m -o=/srv/nominatim/src/data.osm.pbf
+  echo "Filtering places: Converted o5m back to pbf"
+  rm /srv/nominatim/src/data.o5m
+  echo "Filtering places: Deleted o5m"
+  echo "Filtering places: Done"
 fi
