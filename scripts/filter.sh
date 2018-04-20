60261418733db166d1ffff4e7661eab24915e919
Julian Hinz <julianhinz@gmail.com>
2018-04-20 15:11:25 +0200

    different filter

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -2,12 +2,13 @@
 
 if ${FILTER}; then
   echo "Filtering places."
-  osmconvert /srv/nominatim/src/data.osm.pbf -o=/srv/nominatim/src/data_old.o5m
-  echo "Filtering places: Converted pbf to o5m"
+  osmconvert /srv/nominatim/src/data.osm.pbf --drop-version --drop-author -o=/srv/nominatim/src/data_old.o5m
+  echo "Filtering places: Converted pbf to o5m, file size:"
+  ls -lh /srv/nominatim/src/
   rm /srv/nominatim/src/data.osm.pbf
   echo "Filtering places: Deleted pbf"
 #  osmfilter /srv/nominatim/src/data_old.o5m --keep="place=*" --drop-author -o=/srv/nominatim/src/data.o5m
-  osmfilter /srv/nominatim/src/data_old.o5m --drop="building=yes" --drop-author -o=/srv/nominatim/src/data.o5m
+  osmfilter /srv/nominatim/src/data_old.o5m --keep="amenity= boundary= craft= historic= leisure= military= natural= office= place= shop= tourism=" -o=/srv/nominatim/src/data.o5m
   echo "Filtering places: Filtered o5m"
   rm /srv/nominatim/src/data_old.o5m
   echo "Filtering places: Deleted pbf"
